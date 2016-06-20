7a0ae786d63bb801957d6cf90af79f163e08df9f
Jon Seaman <jonmseaman@live.com>
2016-06-20 16:34:31 -0700

    Add project files.

---
+++ EventSimulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
