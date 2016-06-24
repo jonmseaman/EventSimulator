ecc10a8aa8d8eecc841cb453b45da732c28fb0cf
Jon Seaman <jonmseaman@live.com>
2016-06-24 16:27:55 -0700

    SendEvents

--- EventSimulator/Program.cs
+++ EventSimulator/Program.cs
@@ -30,67 +30,60 @@ namespace EventSimulator
                 throw new Exception("Could not find 'ConnectionString' in AppSettings in App.Config");
             }
 
-            int eventsSent = 0;
-
-            var threadStart = new ThreadStart(async () =>
+            const int numThreads = 2;
+            var threads = new Thread[numThreads];
+            for (var i = 0; i < numThreads; i++)
             {
-                // Event sender and receiver.
-                var eventCreator = new EventCreator();
-                var eventSender = new Sender(connectionString);
-
-                // Create list of events to send to eventHub
-                var eventList = new List<Event>();
-                for (var i = 0; i < 512; i++)
-                {
-                    eventList.Add(eventCreator.CreateClickEvent());
-                }
-
-                var timeStarted = DateTime.Now;
-
-                while (true)
-                {
-                    // Update list of events to show user action.
-                    for (var i = 0; i < eventList.Count; i++)
-                    {
-                        eventList[i] = eventCreator.CreateNextEvent(eventList[i], UserBehavior.FastPurchase);
-                    }
-
-                    // Send the events
-                    Console.WriteLine("Sending the results.");
-                    try
-                    {
-                        await eventSender.SendBatchAsync(eventList);
-                        eventsSent += eventList.Count;
-                    }
-                    catch (MessageSizeExceededException e)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("Batch size exceeded. ");
-                        Console.WriteLine(e.Message);
-                        Console.ResetColor();
-                        break;
-                    }
-                    Console.WriteLine("Finished Sending data.");
-                    Console.WriteLine("Total events sent: " + eventsSent);
-                    var seconds = (DateTime.Now - timeStarted).TotalSeconds;
-                    Console.WriteLine("Average events per second: " + eventsSent / seconds);
-                }
-            });
-
-            Thread eventThread = new Thread(threadStart);
-            Thread[] threads = new Thread[12];
-            for (var i = 0; i < 12; i++)
-            {
-                threads[i] = new Thread(threadStart);
+                threads[i] = new Thread(() => SendEvents(connectionString));
                 threads[i].Start();
             }
 
-
-            eventThread.Start();
-
             Console.WriteLine("Press enter to exit...");
             Console.ReadLine();
-            eventThread.Abort();
+            for (var i = 0; i < numThreads; i++)
+            {
+                threads[i].Abort();
+            }
         }
+
+        public static async void SendEvents(string connectionString)
+        {
+            // Event sender and receiver.
+            var eventCreator = new EventCreator();
+            var eventSender = new Sender(connectionString);
+
+            // Create list of events to send to eventHub
+            var eventList = new List<Event>();
+            for (var i = 0; i < 512; i++)
+            {
+                eventList.Add(eventCreator.CreateClickEvent());
+            }
+
+            while (true)
+            {
+                // Update list of events to show user action.
+                for (var i = 0; i < eventList.Count; i++)
+                {
+                    eventList[i] = eventCreator.CreateNextEvent(eventList[i], UserBehavior.FastPurchase);
+                }
+
+                // Send the events
+                Console.WriteLine("Sending the results.");
+                try
+                {
+                    await eventSender.SendBatchAsync(eventList);
+                }
+                catch (MessageSizeExceededException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Batch size exceeded. ");
+                    Console.WriteLine(e.Message);
+                    Console.ResetColor();
+                    break;
+                }
+                Console.WriteLine("Finished Sending data.");
+            }
+        }
+
     }
 }
