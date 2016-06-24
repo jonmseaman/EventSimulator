206d6465c8ca24fab567089483877d6bdf626c7a
Jon Seaman <jonmseaman@live.com>
2016-06-24 10:12:40 -0700

    Copy ctors for ClickEvent

--- EventSimulator/Events/ClickEvent.cs
+++ EventSimulator/Events/ClickEvent.cs
@@ -14,8 +14,35 @@ namespace EventSimulator.Events
         public DateTime EntryTime { get; set; }
         public DateTime ExitTime { get; set; }
 
+        #region Constructors
+
+
         public ClickEvent() : base(EventType.Click)
         { }
 
+        /// <summary>
+        /// Makes a shallow copy of e.
+        /// </summary>
+        /// <param name="e">The event to be copied.</param>
+        public ClickEvent(ClickEvent e): base(e)
+        {
+            PrevUrl = e.PrevUrl;
+            NextUrl = e.NextUrl;
+            EntryTime = e.EntryTime;
+            ExitTime = e.ExitTime;
+        }
+
+        /// <summary>
+        /// Makes a shallow copy, but only copies inherited members.
+        /// </summary>
+        /// <param name="e"> The event to be copied.</param>
+        public ClickEvent(Event e) : base(e)
+        {
+            // Nothing to do here.
+        }
+
+
+
+        #endregion
     }
 }
