9ea9737e2708d73d6bb9aac570b0d55f00ec05b2
Jon Seaman <jonmseaman@live.com>
2016-07-01 10:51:10 -0700

    Allow sending only ClickEvents or PurchaseEvents

--- EventSimulator/Program.cs
+++ EventSimulator/Program.cs
@@ -15,82 +15,75 @@ namespace EventSimulator
 {
     /// <summary>
     /// This program is supposed to simulate usage of an ecommerce website.
-    /// This program sends events corresponding to navigating around the website
-    /// as well as making purchases. The events are sent to an event hub corresponding
-    /// to a connection string stored in the App.config file.
+    /// This program sends simulated events to an event hub according 
+    /// to its App.config and cmd line arguments.
     /// </summary>
-    class Program
+    static class Program
     {
+        #region Member Variables
+
         private static int[] behaviorPercents;
         private static int BatchSize { get; set; } = 512;
 
+        delegate List<Event> CreateListDelegate();
+
+        private static CreateListDelegate CreateList;
+
+        delegate void UpdateListDelegate(List<Event> events);
+
+        static UpdateListDelegate UpdateList;
+
+        #endregion
+
         static void Main(string[] args)
         {
-            // Get distribution of events from the command line parameters.
-            behaviorPercents = new int[3];
+            // Load settings
             try
             {
-                var sum = 0;
-                for (var i = 0; i < 3; i++)
-                {
-                    behaviorPercents[i] = int.Parse(args[i]);
-                    sum += behaviorPercents[i];
-                }
-
-                if (sum != 100)
-                {
-                    throw new ArgumentException("Command line arguments do not add up to 100%.");
-                }
-
+                Settings.Load();
+                behaviorPercents = Settings.BehaviorPercents;
+                BatchSize = Settings.BatchSize;
             }
             catch (Exception e)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(e.Message);
-                Console.WriteLine("Usage:\n" +
-                                  System.AppDomain.CurrentDomain.FriendlyName +
-                                  " %UsersPurchasingFirstProductViewed %UsersPurchasingAfterBrowsing %UsersBrowsing");
-                Console.WriteLine("These must add up to 100%.");
                 Console.ResetColor();
                 Console.WriteLine("Press enter to exit...");
                 Console.ReadLine();
                 Environment.Exit(exitCode: 1);
             }
 
-
-            // Get data from config file.
-            var config = ConfigurationManager.AppSettings;
-            // Get the conenction string from the config file.
-            string connectionString = config["ConnectionString"];
-            if (connectionString == null)
-            { // TODO: What kind of exception should be thrown?
-                throw new Exception("Could not find 'ConnectionString' in AppSettings in App.Config");
-            }
-            // Get batch size from config file.
-            var batchSizeStr = config["BatchSize"];
-            if (batchSizeStr == null)
+            // TODO:
+            // Set up delegates
+            if (args.Length > 0 && args[0].Equals("ClickEvents"))
             {
-                Console.WriteLine($"Couldn't find batch size in App.Config. Using default {BatchSize}.");
-            }
-            int batchSize;
-            if (!int.TryParse(batchSizeStr, out batchSize))
+                CreateList = CreateClickEvents;
+                UpdateList = UpdateClickEvents;
+            } else if (args.Length > 0 && args[0].Equals("PurchaseEvents"))
             {
-                Console.WriteLine($"Couldn't parse BatchSize from App.Config. Using default {BatchSize}.");
+                CreateList = CreatePurchaseEvents;
+                UpdateList = UpdatePurchaseEvents;
             }
             else
             {
-                BatchSize = batchSize;
+                CreateList = CreateClickEvents;
+                UpdateList = UpdateSimulatedEvents;
             }
 
             // Set up threads.
-            var numThreads = Environment.ProcessorCount;
+            var numThreads = Settings.MaxThreads;
+            if (numThreads == 0)
+            {
+                numThreads = Environment.ProcessorCount;
+            }
             var eventsSentByThread = new int[numThreads];
             var threads = new Thread[numThreads];
             var timeStarted = DateTime.Now;
             for (var i = 0; i < numThreads; i++)
             {
                 var i1 = i; // Make sure the lambda gets the right value of i.
-                threads[i] = new Thread(() => SendEvents(connectionString, ref eventsSentByThread[i1]));
+                threads[i] = new Thread(() => SendEvents(Settings.ConnectionString, ref eventsSentByThread[i1]));
                 threads[i].Start();
             }
 
@@ -104,14 +97,7 @@ namespace EventSimulator
                     // Count events sent.
                     var sum = eventsSentByThread.Sum();
                     var eps = sum/(DateTime.Now - timeStarted).TotalSeconds;
-                    Console.Write("Events sent: ");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write(sum.ToString());
-                    Console.ResetColor();
-                    Console.Write(". Events per second: ");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(eps);
-                    Console.ResetColor();
+                    Console.WriteLine($"Events sent: {sum}\tEvents per second: {eps}");
                 }
             });
             countThread.Start();
@@ -119,6 +105,7 @@ namespace EventSimulator
             // Done.
             Console.WriteLine("Press enter to exit...");
             Console.ReadLine();
+            countThread.Abort();
             for (var i = 0; i < numThreads; i++)
             {
                 threads[i].Abort();
@@ -131,26 +118,11 @@ namespace EventSimulator
             var eventSender = new Sender(connectionString);
 
             // Create list of events to send to eventHub
-            var eventList = new List<Event>();
-            for (var i = 0; i < BatchSize; i++)
-            {
-                eventList.Add(EventCreator.CreateClickEvent());
-            }
+            var eventList = CreateList();
 
             while (true)
             {
-                // Update list of events to show user action.
-                var offset = 0;
-                var fastPurchaseCount = behaviorPercents[0] * BatchSize / 100;
-                var slowPurchaseCount = behaviorPercents[1] * BatchSize / 100;
-                var browsingCount = eventList.Count - fastPurchaseCount - slowPurchaseCount;
-                
-                UpdateEvents(offset, fastPurchaseCount, eventList, UserBehavior.FastPurchase);
-                offset += fastPurchaseCount;
-                UpdateEvents(offset, slowPurchaseCount, eventList, UserBehavior.SlowPurchase);
-                offset += slowPurchaseCount;
-                UpdateEvents(offset, browsingCount, eventList, UserBehavior.Browsing);
-
+                UpdateList(eventList);
 
                 // Send the events
                 try
@@ -169,12 +141,74 @@ namespace EventSimulator
             }
         }
 
-        private static void UpdateEvents(int startIndex, int count, List<Event> eventList , UserBehavior behavior)
+        #region CreateEvents
+
+        private static List<Event> CreateClickEvents()
+        {
+            var eventList = new List<Event>();
+            for (var i = 0; i < BatchSize; i++)
+            {
+                eventList.Add(EventCreator.CreateClickEvent());
+            }
+            return eventList;
+        }
+
+        private static List<Event> CreatePurchaseEvents()
+        {
+            var eventList = new List<Event>();
+            for (var i = 0; i < BatchSize; i++)
+            {
+                eventList.Add(EventCreator.CreatePurchaseEvent());
+            }
+            return eventList;
+        }
+
+        #endregion
+
+        #region UpdateEvents
+
+        private static void UpdateSimulatedEvents(List<Event> eventList)
+        {
+            // Update list of events to show user action.
+            var offset = 0;
+            var fastPurchaseCount = behaviorPercents[0] * BatchSize / 100;
+            var slowPurchaseCount = behaviorPercents[1] * BatchSize / 100;
+            var browsingCount = eventList.Count - fastPurchaseCount - slowPurchaseCount;
+
+            UpdateSimulatedEventsWithBehavior(offset, fastPurchaseCount, eventList, UserBehavior.FastPurchase);
+            offset += fastPurchaseCount;
+            UpdateSimulatedEventsWithBehavior(offset, slowPurchaseCount, eventList, UserBehavior.SlowPurchase);
+            offset += slowPurchaseCount;
+            UpdateSimulatedEventsWithBehavior(offset, browsingCount, eventList, UserBehavior.Browsing);
+
+        }
+
+        private static void UpdateSimulatedEventsWithBehavior(int startIndex, int count, List<Event> eventList, UserBehavior behavior)
         {
             for (var i = startIndex; count > 0; count--, i++)
             {
                 eventList[i] = EventCreator.CreateNextEvent(eventList[i], behavior);
             }
         }
+
+        private static void UpdateClickEvents(List<Event> eventList)
+        {
+            for (var i = 0; i < eventList.Count; i++)
+            {
+                eventList[i] = EventCreator.CreateNextClickEvent(eventList[i]);
+            }
+
+        }
+
+        private static void UpdatePurchaseEvents(List<Event> eventList)
+        {
+            for (var i = 0; i < eventList.Count; i++)
+            {
+                eventList[i] = EventCreator.CreateNextPurchaseEvent(eventList[i]);
+            }
+        }
+
+        #endregion
+
     }
 }
---
+++ EventSimulator/Settings.cs
@@ -0,0 +1,91 @@
+﻿using System.Configuration;
+using System.Linq;
+
+namespace EventSimulator
+{
+    internal class Settings
+    {
+        /// <summary>
+        ///     Loads settings from App.Config.
+        /// </summary>
+        public static void Load()
+        {
+            // Make config mngr
+            // Get data from config file.
+            var config = ConfigurationManager.AppSettings;
+            // Get the connection string
+            var connectionString = config["ConnectionString"];
+            if (connectionString != null)
+            {
+                ConnectionString = connectionString;
+            }
+            // Get the batch size.
+            var batchSizeStr = config["BatchSize"];
+            var sz = 0;
+            if (batchSizeStr != null && int.TryParse(batchSizeStr, out sz))
+            {
+                BatchSize = sz;
+            }
+            else
+            {
+                throw new ConfigurationErrorsException("Failed to load BatchSize from App.config.");
+            }
+            // Get the behavior percents.
+            BehaviorPercents = new int[3];
+            var percentStrs = new string[3];
+            percentStrs[0] = config["FastPurchasePercent"];
+            percentStrs[1] = config["SlowPurchasePercent"];
+            percentStrs[2] = config["BrowsingPercent"];
+            if (percentStrs.Contains(null))
+            {
+                throw new ConfigurationErrorsException("Not all behavior percents found in App.config.");
+            }
+            for (var i = 0; i < 3; i++)
+            {
+                BehaviorPercents[i] = int.Parse(percentStrs[i]);
+            }
+
+            if (BehaviorPercents.Sum() != 100)
+            {
+                throw new ConfigurationErrorsException("Behavior percents in App.config don't add up to 100.");
+            }
+            // Get events per second
+            var eventsPerSecond = config["EventsPerSecond"];
+            if (eventsPerSecond == null)
+            {
+                throw new ConfigurationErrorsException("EventsPerSecond not found in App.config.");
+            }
+            EventsPerSecond = int.Parse(eventsPerSecond);
+            // MaxThreads
+            var maxThreadsStr = config["MaxThreads"];
+            int maxThreads;
+            if (maxThreadsStr != null && int.TryParse(maxThreadsStr, out maxThreads))
+            {
+                MaxThreads = maxThreads;
+            }
+            else
+            {
+                throw new ConfigurationErrorsException("MaxThreads not found in App.config.");
+            }
+
+        }
+
+        #region Settings
+
+        public static int BatchSize { get; set; } = 512;
+        public static string ConnectionString { get; set; }
+
+        /// <summary>
+        ///     [0] - FastPurchase
+        ///     [1] - SlowPurchase
+        ///     [2] - Browsing
+        /// </summary>
+        public static int[] BehaviorPercents { get; set; }
+
+        public static int EventsPerSecond { get; set; }
+
+        public static int MaxThreads { get; set; }
+
+        #endregion
+    }
+}
