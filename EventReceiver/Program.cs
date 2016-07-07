b621f2426fd7f727f5a82f9f454d179f4aef7a07
Jon Seaman <jonmseaman@live.com>
2016-07-07 14:28:49 -0700

    Added receiver and settings to receiver.

---
+++ EventReceiver/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = "Endpoint=sb://servicebusintern2016.servicebus.windows.net/;SharedAccessKeyName=Managed;SharedAccessKey=oRt/agurnQYUDRsvm0tOKOEi2e5nXr0MnTrUxP8PdTw=";
+            string eventHubName = "sellersite";
+            string storageAccountName = "sbi2016ehreceiver";
+            string storageAccountKey = "ijLlpwJryEx+o3dJIn2NDoUcvzZwVkhVSuPpQJezZjcnSGgVrwzOzpwaYF55b45AC8olef6MiltcB202mBMo4g==";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+            Console.WriteLine("Enter the consumer group name: ");
+            string consumerGroupName = Console.ReadLine();
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, consumerGroupName, eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(options).Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
---
+++ EventReceiver/Settings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Receiver
+{
+    internal class Settings
+    {
+        #region Settings
+            
+            public string ConnectionString { get; set; }
+
+            public string ConsumerGroup { get; set; }
+
+            public bool IsFirstRun { get; set; }
+
+        #endregion
+
+        public void Load() {
+            // Get data from the config file.
+            var config = ConfigurationManager.AppSettings;
+
+            // Get the connection string
+            var connectionString = config["ConnectionString"];
+            if (connectionString != null)
+            {
+                ConnectionString = connectionString;
+            }
+
+            // Get the consumer group.
+            var consumerGroup = config["ConsumerGroup"];
+            if (consumerGroup != null)
+            {
+                ConsumerGroup = consumerGroup;
+            }
+
+            // IsFirstRun
+            var isFirstRunStr = config["IsFirstRun"];
+            bool isFirstRun;
+            if (bool.TryParse(isFirstRunStr, out isFirstRun)) {
+                IsFirstRun = isFirstRun;
+            }
+        }
+
+        public void Save() {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var app = config.AppSettings.Settings;
+
+            // Add settings.
+            app.Clear();
+            app.Add("ConnectionString", ConnectionString);
+            app.Add("ConsumerGroup", ConsumerGroup);
+            app.Add("IsFirstRun", IsFirstRun.ToString());
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+    }
+}
